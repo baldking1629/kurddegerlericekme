5189f01043826bf8f5b1691d822fefefe65fc22a
Mahmut KELES <mahmutt.keles38@gmail.com>
2022-12-02 15:36:08 +0300

    veriler dosyaya yazıldı

--- kurdegerlericekme.py
+++ kurdegerlericekme.py
@@ -2,6 +2,12 @@ from forex_python.converter import CurrencyRates
 
 c = CurrencyRates()
 dolarkuru = c.get_rate('USD','TRY')
-eurokuru = c.get_rates('TRY')
+eurokuru = c.get_rate('EUR','TRY')
 print("Anlık dolar kuru : ",dolarkuru)
 print("Anlık euro kuru : ",eurokuru)
+dolarkuru = round(dolarkuru,4)
+with open ("kurdegerleri.txt","a") as myfile:
+    myfile.write("Dolar kuru : "+str(dolarkuru))
+    myfile.write("\n")
+    myfile.write("Euro kuru : "+str(eurokuru))
+    myfile.write("\n")
